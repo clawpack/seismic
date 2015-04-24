3a63396ec0e1e152aa7153b648304588b87e7dde
cvogl@u.washington.edu <cvogl@u.washington.edu>
2015-04-23 20:29:00 -0700

    Added regions to flag2refine along with a check that looks for the infinity norm of the stress tensor being over the specified tolerance

--- src/3d/flag2refine.f90
+++ src/3d/flag2refine.f90
@@ -24,26 +24,61 @@ subroutine flag2refine(mx,my,mz,mbc,meqn,maux,xlower,ylower, &
 !  q(3) - sigma_zz
 ! ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 !
+    use regions_module, only: regions, num_regions
+
     implicit none
 
-
-    real (kind=8) :: q(meqn,1-mbc:mx+mbc,1-mbc:my+mbc,1-mbc:mz+mbc)
-    real (kind=8) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc,1-mbc:mz+mbc)
-    real (kind=8) :: amrflags(1-mbc:mx+mbc,1-mbc:my+mbc,1-mbc:mz+mbc)
+    integer, intent(in) :: mx, my, mz, mbc, meqn, maux
+    real (kind=8), intent(in) :: q(meqn,1-mbc:mx+mbc,1-mbc:my+mbc,1-mbc:mz+mbc)
+    real (kind=8), intent(in) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc,1-mbc:mz+mbc)
+    real (kind=8), intent(inout) :: amrflags(1-mbc:mx+mbc,1-mbc:my+mbc,1-mbc:mz+mbc)
     logical     allowflag
     external    allowflag
-    real (kind=8) :: DOFLAG, DONTFLAG, xlower, ylower, zlower, dx, dy, dz, tolsp, t
-    integer :: mx, my, mz, mbc, meqn, maux, i, j, k
+    real (kind=8), intent(in) :: DOFLAG, DONTFLAG, xlower, ylower, zlower, dx, dy, dz, tolsp, t, level
+
+    real (kind=8) :: xcell, ycell, zcell, max_stress
+    integer :: i, j, k, m, min_level, max_level
+    integer :: infinity = 1e3
 
 !   # loop over interior points on this grid:
     do k = 1,mz
+        zcell = zlower + (k-0.5d0)*dz
         do j = 1,my
+            ycell = ylower + (j-0.5d0)*dy
             do i = 1,mx
+                xcell = xlower + (i-0.5d0)*dx
 
-                amrflags(i,j,k) = DONTFLAG
-                if (allowflag(x,y,z,t,level) .and. dabs(q(1,i,j,k) + q(2,i,j,k) &
-                    + q(3,i,j,k))/3.d0 .ge. tolsp) then
+!               # check which regions, if any, the point is in
+                min_level = 0
+                max_level = infinity
+                do m =1,num_regions
+                    if (regions(m)%t_low .le. t .and. t .le. regions(m)%t_hi .and. &
+                        regions(m)%x_low .le. xcell .and. xcell .le. regions(m)%x_hi .and. &
+                        regions(m)%y_low .le. ycell .and. ycell .le. regions(m)%y_hi .and. &
+                        regions(m)%z_low .le. zcell .and. zcell .le. regions(m)%z_hi) then
+                        min_level = max(min_level, regions(m)%min_level)
+                        max_level = min(max_level, regions(m)%max_level)
+                    end if
+                end do
+
+!               # if point is in region, make sure that region is refined as specified
+!               # if no problems, use specified tolerance and stress
+                if (min_level > 0 .and. level < min_level) then
                     amrflags(i,j,k) = DOFLAG
+                else if (min_level > 0 .and. max_level < level) then
+                    amrflags(i,j,k) = DONTFLAG
+                else if (allowflag(xcell,ycell,zcell,t,level)) then
+                    max_stress = 0.d0
+                    do m = 1,6
+                        max_stress = max(max_stress, dabs(q(m,i,j,k)))  
+                    end do
+                    if (max_stress .ge. tolsp) then
+                        amrflags(i,j,k) = DOFLAG
+                    else
+                        amrflags(i,j,k) = DONTFLAG
+                    end if
+                else
+                    amrflags(i,j,k) = DONTFLAG
                 end if
             
             end do
