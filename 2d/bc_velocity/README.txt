edb77a0426deb1b1e6626bd3088722f729b32649
Randy LeVeque <rjl@uw.edu>
2016-02-27 10:08:27 -0800

    initial test specifying slip velocity at lower boundary, run with amrclaw@826b95c0df7cd

---
+++ 2d/bc_velocity/README.txt
@@ -0,0 +1,5 @@
+
+Initial test of setting tangential velocity at lower boundary.
+
+process_gauges.py integrated velocities at gauges to get displacements.
+Set to use gauges at top or bottom of domain.
---
+++ 2d/bc_velocity/setprob.f
@@ -0,0 +1,41 @@
+      subroutine setprob
+      implicit double precision (a-h,o-z)
+      character*12 fname
+      common /comaux/ rho1,amu1,alam1,rho2,amu2,alam2,rho3,amu3,alam3
+      common /combc/ t0wall,tperiod,pi2,amplitude
+
+c
+c     # Set the material parameters for the elasticity equations
+c
+c
+      iunit = 7
+      fname = 'setprob.data'
+c     # open the unit with new routine from Clawpack 4.4 to skip over
+c     # comment lines starting with #:
+      call opendatafile(iunit, fname)
+                
+
+c
+c     # Piecewise constant medium 
+c     # Material parameters
+
+      read(7,*) rho1
+      read(7,*) alam1
+      read(7,*) amu1
+
+      read(7,*) rho2
+      read(7,*) alam2
+      read(7,*) amu2
+
+      read(7,*) rho3
+      read(7,*) alam3
+      read(7,*) amu3
+
+      read(7,*) t0wall
+      read(7,*) tperiod
+      read(7,*) amplitude
+      pi2 = 8.d0*datan(1.d0)
+
+      return
+      end
+
