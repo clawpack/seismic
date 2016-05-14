3ff613eff07152298e970c0d4bcc4696c094065b
cvogl@u.washington.edu <cvogl@u.washington.edu>
2016-05-14 16:18:27 -0700

    Added water layer.  Results not as expected, so may need more testing

---
+++ 2d/sloping_fault_water/mapc2p.f90
@@ -0,0 +1,46 @@
+  !=====================================================
+  subroutine mapc2p(xc,yc,xp,yp)
+  !=====================================================
+    ! Maps for sloping fault
+    ! on input,  (xc,yc) is a computational grid point
+    ! on output, (xp,yp) is corresponding point in physical space
+
+    implicit none
+    real (kind=8), intent(in) :: xc,yc
+    real (kind=8), intent(out) :: xp,yp
+
+    ! Variables from setprob:
+    real (kind=8) :: center(2), theta, xcb(2), mindepth
+    common /fault/  center, theta, xcb, mindepth
+
+    real (kind=8) :: scaling
+    common /water/  scaling
+
+    ! Local variables
+    real (kind=8) :: ls, alpha, xrot, yrot
+
+    if (xc < xcb(1)) then
+      ls = dsqrt((xc-xcb(1))**2 + (yc-center(2))**2)
+    elseif (xc > xcb(2)) then
+      ls = dsqrt((xc-xcb(2))**2 + (yc-center(2))**2)
+    else
+      ls = dabs(yc - center(2))
+    end if
+
+    alpha = ls/mindepth
+    xrot = center(1) + dcos(theta)*(xc-center(1)) - dsin(theta)*(yc-center(2))
+    yrot = center(2) + dsin(theta)*(xc-center(1)) + dcos(theta)*(yc-center(2))
+
+    if (yc > 0) then
+      xp = xc
+      yp = yc*scaling
+    elseif (alpha < 1.d0) then
+      xp = (1.d0-alpha)*xrot + alpha*xc
+      yp = (1.d0-alpha)*yrot + alpha*yc
+    else
+      xp = xc
+      yp = yc
+    end if
+
+    return
+    end
---
+++ 2d/sloping_fault_water/plot_okada.py
@@ -0,0 +1,62 @@
+from pylab import *
+import dtopotools_horiz_okada as dtopotools
+reload(dtopotools)
+fault = dtopotools.Fault()
+fault.subfaults = []
+
+width = sqrt(50e3**2 + 8.6e3**2)
+
+ndip = 50
+dlongitude = (50e3/111.e3) / ndip   # convert to degees and split up
+
+xx = arange(0,50e3,50e3/ndip)
+if 1:
+    slip = exp(-((xx-25e3)/12e3)**2)
+else:
+    loadtxt('slip.txt')
+
+for i in range(ndip):
+    subfault = dtopotools.SubFault()
+    subfault.mu = 3e10
+    subfault.dip = 9.9
+    subfault.width = width / ndip
+    x = i*50e3/ndip
+    subfault.depth = +15e3 + x*sin(subfault.dip * pi/180.)
+    subfault.slip = slip[i]
+    subfault.rake = 90
+    subfault.strike = 0
+    subfault.length = 1000e3
+    subfault.longitude = i * dlongitude
+    subfault.latitude = 0.
+    subfault.coordinate_specification = 'top center'
+    subfault.calculate_geometry()
+
+    fault.subfaults.append(subfault)
+
+print "Mw = ", fault.Mw()
+
+x = linspace(-1.5,1.5,1000)
+y = array([0.])
+dtopo = fault.create_dtopography(x,y,[1.],y_disp=True)
+
+def plot_okada_surface(ax=None, plotstyle='k-'):
+    if ax is None:
+        figure()
+        ax = subplot(111)
+    ax.plot(dtopo.x*111e3,dtopo.dZ[0,0,:],plotstyle,label="Okada")
+
+def plot_okada_horiz(ax=None, plotstyle='k-'):
+    if ax is None:
+        figure()
+        ax = subplot(111)
+    ax.plot(dtopo.x*111e3,dtopo.dY[0,0,:],plotstyle,label="Okada")
+
+if __name__=='__main__':
+    figure()
+    ax = subplot(211)
+    plot_okada_surface(ax)
+    ax = subplot(212)
+    plot_okada_horiz(ax)
+    savefig('okada.png')
+    print "Created okada.png"
+
